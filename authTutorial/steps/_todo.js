520333b0dcf7373e53267e3573a654708d65ab37
Matt Olsen <Matt.Olsen@elevationscu.com>
2024-02-16 12:04:52 -0700

    added a todo list

---
+++ authTutorial/steps/_todo.js
@@ -0,0 +1,11 @@
+// add an expiration to the cookie
+// explain cookies are stored on the client and sessions are stored on the server.
+// store secrets in the .env file
+// make a script to go get the secrets stored somewhere else before the server is started
+// register endpoint
+// password reset endpoint
+// Terraform a server
+// Terraforma  database
+// make an actual frontend page for each route
+// Hook up a real database to the session store
+// hook up a real db for the users table
